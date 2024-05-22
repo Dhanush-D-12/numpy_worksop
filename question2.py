9c994d3b1e887cebd61cd759aec8444f9dfedbd7
Dhanush-D-12 <155568491+Dhanush-D-12@users.noreply.github.com>
2024-05-22 11:02:31 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,13 @@
 # find if the given number is a palindrome or not?
+def is_palindrome(s):
+    s = s.replace(" ", "").lower()  # remove spaces and convert to lowercase
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+n = input("Enter a string:")
+print(is_palindrome(n))
